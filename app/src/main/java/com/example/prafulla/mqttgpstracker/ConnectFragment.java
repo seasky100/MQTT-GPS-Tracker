9783f81c9703cb9322eea36abe412dc4321e0788
PrafullaChaudhari <prafullaachaudhari@gmail.com>
2017-05-06 09:39:22 +0530

    Completed MQTT Subscribe functionality

--- app/src/main/java/com/example/prafulla/mqttgpstracker/ConnectFragment.java
+++ app/src/main/java/com/example/prafulla/mqttgpstracker/ConnectFragment.java
@@ -7,12 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConnectFragment extends Fragment {
-
 
     public ConnectFragment() {
         // Required empty public constructor
@@ -23,6 +21,13 @@ public class ConnectFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+/*        Bundle arguments = new Bundle();
+        arguments.putBoolean("IsConnected", true);
+        MessagesFragment fragment = new MessagesFragment();
+        fragment.setArguments(arguments);*/
+
         return inflater.inflate(R.layout.fragment_connect, container, false);
     }
 
--- app/src/main/java/com/example/prafulla/mqttgpstracker/WelcomeActivity.java
+++ app/src/main/java/com/example/prafulla/mqttgpstracker/WelcomeActivity.java
@@ -1,25 +1,42 @@
 package com.example.prafulla.mqttgpstracker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    public static MqttAndroidClient client;
     private Fragment fragment;
     private FragmentManager fragmentManager;
+    private String topic;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            ((TextView)findViewById(R.id.welcomeMsg)).setText("");
 
             fragmentManager = getSupportFragmentManager();
 
@@ -48,6 +65,123 @@ public class WelcomeActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        try
+        {
+            SharedPreferences sharedPrefs = this.getSharedPreferences("MGTPrefsFile",0);;
+
+            String server = sharedPrefs.getString("server", "");
+            String port = sharedPrefs.getString("port", "");
+            String uname = sharedPrefs.getString("uname", "");
+            String passw = sharedPrefs.getString("passw", "");
+            topic = sharedPrefs.getString("topic", "") + "/#";
+
+            if (server.isEmpty() || port.isEmpty() || uname.isEmpty() || passw.isEmpty() || topic.isEmpty())
+            {
+                Toast.makeText(getApplicationContext(), "Please Enter MQTT Server Details", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                String uri = "tcp://" + server + ":" + port;
+
+                //String clientId = MqttClient.generateClientId();
+                String clientId = "MGT" + System.nanoTime();
+
+                MqttConnectOptions conOpt = new MqttConnectOptions();
+                conOpt.setCleanSession(false);
+                conOpt.setConnectionTimeout(10);
+                //conOpt.setKeepAliveInterval(30);
+                conOpt.setUserName(uname);
+                conOpt.setPassword(passw.toCharArray());
+
+                client = new MqttAndroidClient(this, uri, clientId);
+
+                client.setTraceEnabled(true);
+                client.setCallback(new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable cause) {
+                        Toast.makeText(getApplicationContext(), "The Connection is lost..", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void messageArrived(String topic, MqttMessage message) throws Exception {
+                        if(message != null) {
+                            Toast.makeText(getApplicationContext(), "Message Received", Toast.LENGTH_SHORT).show();
+                            ((TextView) findViewById(R.id.welcomeMsg)).setText(message.toString());
+                        }
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken token) {
+
+                    }
+                });
+
+                try {
+                    client.connect(conOpt, null, new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            //DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                            //disconnectedBufferOptions.setBufferEnabled(true);
+                            //disconnectedBufferOptions.setBufferSize(100);
+                            //disconnectedBufferOptions.setPersistBuffer(false);
+                           // disconnectedBufferOptions.setDeleteOldestMessages(false);
+                           // mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                            subscribeToTopic();
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            Toast.makeText(getApplicationContext(), "Failed to connect to server..", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                catch (MqttException e) {
+                    Log.e(this.getClass().getCanonicalName(),
+                            "MqttException Occured", e);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        try {
+            if(client != null && client.isConnected()) {
+                client.disconnect();
+            }
+        }
+        catch (MqttException e) {
+            Log.e(this.getClass().getCanonicalName(),
+                    "MqttException Occured", e);
+        }
+        super.onBackPressed();
+    }
+
+    public void subscribeToTopic(){
+        try {
+            String[] topics = new String[1];
+
+            client.subscribe(topic, 1, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(getApplicationContext(), "Subscribed", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(getApplicationContext(), "Failed to subscribe..", Toast.LENGTH_LONG).show();
+                }
+            });
+
+
+        } catch (MqttException ex){
+            System.err.println("Exception while subscribing");
+            ex.printStackTrace();
+        }
     }
 
 }
